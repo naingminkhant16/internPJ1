d71df0c6daeb040544a4bbe0e0b54d8d8e985b56
naingminkhant16 <tharkhant777@gmail.com>
2022-02-28 10:57:37 +0630

    modify and submit commit

--- js/index.js
+++ js/index.js
@@ -20,11 +20,11 @@ document.addEventListener("DOMContentLoaded", function () {
         scrollTopbtn();
         // reveal elements 
         for (let i = 0; i < reveals.length; i++) {
-            
+
             var windowHeight = window.innerHeight;
             var revealTop = reveals[i].getBoundingClientRect().top;
             var revealPoint = 150;
-            
+
             if (revealTop < windowHeight - revealPoint) {
                 reveals[i].classList.add('active');
             } else {
